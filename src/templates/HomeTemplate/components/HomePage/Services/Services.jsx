f101f99b1fc1b8c9a78e47581a365e9e96f104e2
daotienluc <lucd1177@gmail.com>
2024-12-02 11:39:57 +0700

    update UI homepage

--- src/templates/HomeTemplate/components/HomePage/Services/Services.jsx
+++ src/templates/HomeTemplate/components/HomePage/Services/Services.jsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Services.scss";
 import ServicesItem from "./ServicesItem";
 import {
   FundProjectionScreenOutlined,
   AntDesignOutlined,
+  SketchOutlined,
+  SearchOutlined,
+  PictureOutlined,
+  ScheduleOutlined,
+  QuestionOutlined,
 } from "@ant-design/icons";
+import { SkillServices } from "./../../../../../services/skill.services";
+
+const skillIcons = {
+  JavaScript: <FundProjectionScreenOutlined />,
+  Python: <SketchOutlined />,
+  Java: <SearchOutlined />,
+  SQL: <PictureOutlined />,
+  HTML: <ScheduleOutlined />,
+  CSS: <ScheduleOutlined />,
+  Ruby: <ScheduleOutlined />,
+};
 
 const Services = () => {
+  const [skill, setSkill] = useState([]);
+  useEffect(() => {
+    SkillServices.skill()
+      .then((res) => {
+        console.log(res.data.content);
+        setSkill(res.data.content);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="container">
       <div className="grid grid-cols-9 gap-5">
-        <ServicesItem
-          icon={<FundProjectionScreenOutlined />}
-          content="Programming & Tech"
-        />
-        <ServicesItem
-          icon={<AntDesignOutlined />}
-          content="Graphics & Design"
-        />
-        <ServicesItem
-          icon={<AntDesignOutlined />}
-          content="Graphics & Design"
-        />
+        {skill.splice(0, 9).map((item, index) => (
+          <div key={index}>
+            <ServicesItem
+              icon={skillIcons[item.tenSkill] || <QuestionOutlined />}
+              content={item.tenSkill}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
