30b36fb6a132d2d92fdb9ebd6698661279d79f71
daotienluc <lucd1177@gmail.com>
2024-12-04 21:11:13 +0700

    update source

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["yup"], // Đảm bảo Yup được coi là external module
-    },
-  },
 });
